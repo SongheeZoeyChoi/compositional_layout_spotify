aaa9affc5a726b914086aeea15d3ec01ad38f302
songhee <csh0801@dholic.com>
2022-07-06 18:03:56 +0900

    complete and check homework

--- SpotifyPaywall/BannerCell.swift
+++ SpotifyPaywall/BannerCell.swift
@@ -9,4 +9,19 @@ import UIKit
 
 class BannerCell: UICollectionViewCell {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = 16
+    }
+    
+    func configure(_ info: BannerInfo) {
+        titleLabel.text = info.title
+        descLabel.text = info.description
+        thumbnailImageView.image = UIImage(named: info.imageName)
+    }
 }
